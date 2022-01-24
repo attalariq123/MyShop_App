af6d457e6b03e29d8f61b99d6f7399a26f3fc8ed
Muhammad Attalariq <attalariq002@gmail.com>
2022-01-24 12:05:45 +0700

    fix splash web

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shop_app/constant.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -14,29 +16,45 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: colorCustom,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width * 0.5,
-              height: MediaQuery.of(context).size.height * 0.5,
-              child: new Image.asset(
-                "assets/images/farmer_ccexpress.png",
-                fit: BoxFit.cover,
-              ),
-            ),
-            Text(
-              'TaniKu',
-              style: Theme.of(context).textTheme.headline4!.copyWith(
-                    fontSize: 44,
-                    letterSpacing: 1.4,
-                    fontWeight: FontWeight.w900,
-                    color: Colors.white,
+          child: !kIsWeb
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: new Image.asset(
+                        "assets/images/farmer_ccexpress.png",
+                        fit: BoxFit.scaleDown,
+                      ),
+                    ),
+                    Text(
+                      'TaniKu',
+                      style: GoogleFonts.poppins(
+                        fontSize: 44,
+                        letterSpacing: 1.4,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                )
+              : Column(children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.8,
+                    child: new Image.asset(
+                      "assets/images/farmer_ccexpress.png",
+                      fit: BoxFit.fitHeight,
+                    ),
                   ),
-            ),
-          ],
-        ),
-      ),
+                  Text(
+                    'TaniKu',
+                    style: GoogleFonts.poppins(
+                      fontSize: 44,
+                      letterSpacing: 1.4,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ])),
     );
   }
 }
